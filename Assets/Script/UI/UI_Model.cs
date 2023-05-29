6dce350440e34098c46d1085e083aca86fb12dfe
mitsuo_adachi <aphxtwins320@gmail.com>
2023-05-30 05:49:52 +0900

    230530 ATBゲージ機能の追加

--- Assets/Script/UI/UI_Model.cs
+++ Assets/Script/UI/UI_Model.cs
@@ -2,8 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UniRx;
+using UnityEngine.UI;
 
 public class UI_Model : MonoBehaviour
 {
     public ReactiveProperty<float>currentAtb = new FloatReactiveProperty();
+
+    private const int maxAtb = 1;
+    [SerializeField]
+    private float plusAtb = 0.2f;
+
+    [SerializeField]
+    private Button attackButton;
+
+    private void Start()
+    {
+        IncreaseAtbGauge();
+        ActiveAttackButton();
+    }
+
+    /// <summary>
+    ///currentAtbがmaxAtb以下の場合、 currentAtbにplusAtbの値が加算され続ける
+    /// </summary>
+    private void IncreaseAtbGauge()
+    {
+        Observable.EveryUpdate()
+            .Where(_ => currentAtb.Value < maxAtb)
+            .Subscribe(_ => currentAtb.Value += plusAtb * Time.deltaTime)
+            .AddTo(this);
+    }
+
+    /// <summary>
+    /// currentAtbがmaxAtb以上になったらattackButtonをアクティブ状態にする
+    /// </summary>
+    private void ActiveAttackButton()
+    {
+        currentAtb
+            .Select(atb => atb >= maxAtb)
+            .SubscribeToInteractable(attackButton)
+            .AddTo(this);
+    }
 }
--- Assets/Script/UI/UI_View.cs
+++ Assets/Script/UI/UI_View.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UniRx;
 
 public class UI_View : MonoBehaviour
 {
